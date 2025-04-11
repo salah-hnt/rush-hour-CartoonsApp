3604f0790da3a3ee21ed1a0f690b0b231c964fe2
salah-hnt <salah.imoussaine@gmail.com>
2025-04-11 16:25:04 +0200

    add layout-hover

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,20 @@ function App() {
     <main>
        <Outlet />
     </main>
+    <br />
+    <br />
+    <br />
+    <footer>
+      <p>© 2025 Rush-hour Salah - All rights not reserved.</p>
+     
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/Contact">Contact</a></li>
+          <li> <p><a href="https://github.com/salah-hnt" target="_blank">Mon Github</a></p></li>
+        </ul>
+      </nav>
+    </footer>
     </>
   )
 }
